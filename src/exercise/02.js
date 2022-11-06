2ea29b3d5127fbd7d56c29d386de859cedf9c868
ryancolorcafe <ryan.brown@fastradius.com>
2022-11-06 15:10:12 -0800

    if child type is a string, just return the element

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,9 +8,11 @@ function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  return React.Children.map(children, child =>
-    React.cloneElement(child, {on, toggle}),
-  )
+  return React.Children.map(children, child => {
+    return typeof child.type === 'string'
+      ? child
+      : React.cloneElement(child, {on, toggle})
+  })
 }
 
 const ToggleOn = ({on, children}) => {
@@ -31,6 +33,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>!</span>
         <ToggleButton />
       </Toggle>
     </div>
